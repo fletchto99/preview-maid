ff50d1f2e404a4f0023393245e357c955ad3ee45
Matt Langlois <fletchto99@gmail.com>
2024-12-30 01:26:38 +0000

    Better logging

--- previewmaid.py
+++ previewmaid.py
@@ -3,30 +3,18 @@ import schedule
 import time
 import signal
 import sys
+from datetime import datetime
 from plexapi.server import PlexServer
 
-# Get environment variables with default values
-PLEX_URL = os.getenv("PLEX_URL")
-PLEX_TOKEN = os.getenv("PLEX_TOKEN")
-RUN_ONCE = os.getenv("RUN_ONCE", "False").lower() in ("true", "1", "t")
-
-if not PLEX_URL or not PLEX_TOKEN:
-    print("Please set the PLEX_URL and PLEX_TOKEN environment variables.")
-    exit()
-
-def exit_gracefully():
-    print('Received signal to terminate. Exiting gracefully...')
-    sys.exit(0)
-
-signal.signal(signal.SIGTERM, exit_gracefully)
-signal.signal(signal.SIGINT, exit_gracefully)
+def log(message):
+    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - {message}")
 
 def test_plex_connection(plex):
     try:
         server_name = plex.friendlyName
-        print(f"Successfully connected to Plex server: {server_name}")
+        log(f"Successfully connected to Plex server: {server_name}")
     except Exception as e:
-        print(f"Failed to connect to Plex server: {e}")
+        log(f"Failed to connect to Plex server: {e}")
         sys.exit(1)
 
 def process_library(library):
@@ -34,23 +22,41 @@ def process_library(library):
         if item.type == 'show':
             for episode in item.episodes():
                 if not episode.hasPreviewThumbnails:
-                    print(f"{item.title} - {episode.title} (Season {episode.parentIndex}, Episode {episode.index}) "
+                    log(f"{item.title} - {episode.title} (Season {episode.parentIndex}, Episode {episode.index}) "
                           f"has missing preview thumbnails.")
         elif not item.hasPreviewThumbnails:
-            print(f"{item.title} has missing preview thumbnails.")
+            log(f"{item.title} has missing preview thumbnails.")
 
 def find_missing_previews(libraries):
+    log("Searching for missing previews...")
     for library in libraries:
         if library.type == 'show' or library.type == 'movie':
             process_library(library)
+    log("Missing preview run finished.")
+
+# Get environment variables with default values
+PLEX_URL = os.getenv("PLEX_URL")
+PLEX_TOKEN = os.getenv("PLEX_TOKEN")
+RUN_ONCE = os.getenv("RUN_ONCE", "False").lower() in ("true", "1", "t")
+
+if not PLEX_URL or not PLEX_TOKEN:
+    log("Please set the PLEX_URL and PLEX_TOKEN environment variables.")
+    exit()
+
+def exit_gracefully():
+    log('Received signal to terminate. Exiting gracefully...')
+    sys.exit(0)
+
+signal.signal(signal.SIGTERM, exit_gracefully)
+signal.signal(signal.SIGINT, exit_gracefully)
 
 plex = PlexServer(PLEX_URL, PLEX_TOKEN, timeout=600)
 
-print("Testing connection to Plex server...")
+log("Testing connection to Plex server...")
 test_plex_connection(plex)
-print("Preview Maid is running the bootup run...")
+log("Preview Maid will now commence a bootup run..")
 find_missing_previews(plex.library.sections())
-print("Bootup run complete. Preview Maid is now going to run nightly at 00:00.")
+log("Preview Maid is now going to run nightly at 00:00.")
 
 if RUN_ONCE:
     exit()
@@ -59,4 +65,4 @@ schedule.every().day.at("00:00").do(lambda: find_missing_previews(plex.library.s
 
 while True:
     schedule.run_pending()
-    time.sleep(1)
+    time.sleep(600)
