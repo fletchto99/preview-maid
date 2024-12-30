e5f7ebfa2f5d6bab7f8746c63fb87a1963052d1c
Matt Langlois <fletchto99@gmail.com>
2024-12-30 09:42:17 +0000

    - Recursivly process photos - Add the ability to skip libraries of a certain type - Add the ability to skip libraries with a certan name

--- previewmaid.py
+++ previewmaid.py
@@ -23,30 +23,48 @@ def is_preview_thumbnails_enabled(library):
             return setting.value
     return False
 
-def process_library(library):
+def process_photos(album, path=''):
+    for album in album.albums():
+        process_photos(album, f"{path}/{album.title}")
+    for clip in album.clips():
+        if not clip.media[0].parts[0].hasPreviewThumbnails:
+            log(f"{clip.media[0].parts[0].file} is missing preview thumbnails")
+
+def process_library(library, skip_library_types, skip_library_names):
     if not is_preview_thumbnails_enabled(library):
-        log(f"Skipping {library.title}...")
+        log(f"Skipping {library.title} as preview generation is disabled...")
         return
-    log(f'Processing {library.title}...')
+    if library.type in skip_library_types:
+        log(f"Skipping {library.title} as {library.type} is in the SKIP_LIBRARY_TYPES list...")
+        return
+    if library.title in skip_library_names:
+        log(f"Skipping {library.title} as {library.title} is in the SKIP_LIBRARY_NAMES list...")
+        return
+    log(f'Processing {library.title} of type {library.type}...')
     for item in library.all():
         if item.type == 'show':
             for episode in item.episodes():
                 if not episode.hasPreviewThumbnails:
                     log(f"{item.title} - {episode.title} (Season {episode.parentIndex}, Episode {episode.index}) "
-                          f"has missing preview thumbnails.")
-        elif not item.hasPreviewThumbnails:
-            log(f"{item.title} has missing preview thumbnails.")
+                          f"is missing preview thumbnails.")
+        elif item.type == 'movie':
+            if not item.hasPreviewThumbnails:
+                log(f"{item.title} is missing preview thumbnails")
+        elif item.type == 'photo':
+            process_photos(item, item.title)
 
-def find_missing_previews(libraries):
+def find_missing_previews(libraries, skip_library_types, skip_library_names):
     log("Searching for missing previews...")
     for library in libraries:
-        process_library(library)
+        process_library(library, skip_library_types, skip_library_names)
     log("Missing preview run finished.")
 
 # Get environment variables with default values
 PLEX_URL = os.getenv("PLEX_URL")
 PLEX_TOKEN = os.getenv("PLEX_TOKEN")
 RUN_ONCE = os.getenv("RUN_ONCE", "False").lower() in ("true", "1", "t")
+SKIP_LIBRARY_TYPES = os.getenv("SKIP_LIBRARY_TYPES", "").split(",")
+SKIP_LIBRARY_NAMES = os.getenv("SKIP_LIBRARY_NAMES", "").split(",")
 
 if not PLEX_URL or not PLEX_TOKEN:
     log("Please set the PLEX_URL and PLEX_TOKEN environment variables.")
@@ -66,12 +84,12 @@ test_plex_connection(plex)
 
 if RUN_ONCE:
     log("Preview Maid is running in one-time mode...")
-    find_missing_previews(plex.library.sections())
+    find_missing_previews(plex.library.sections(), SKIP_LIBRARY_TYPES, SKIP_LIBRARY_NAMES)
     log("Run completed. Exiting...")
     exit()
 else:
     log("Preview Maid is scheduled to run nightly at 00:00.")
-    schedule.every().day.at("00:00").do(lambda: find_missing_previews(plex.library.sections()))
+    schedule.every().day.at("00:00").do(lambda: find_missing_previews(plex.library.sections(), SKIP_LIBRARY_TYPES, SKIP_LIBRARY_NAMES))
 
     while True:
         schedule.run_pending()
